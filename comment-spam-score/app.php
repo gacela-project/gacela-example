bdde92f65e9bf09712a8d2dee8b13cb3bc4a295b
Chemaclass <chemaclass@outlook.es>
2022-05-02 19:23:37 +0200

    Update gacela 0.17.1

--- comment-spam-score/app.php
+++ comment-spam-score/app.php
@@ -6,9 +6,16 @@ declare(strict_types=1);
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Comment\CommentFacade;
+use Gacela\Framework\Config\GacelaConfigBuilder\ConfigBuilder;
 use Gacela\Framework\Gacela;
+use Gacela\Framework\Setup\SetupGacela;
 
-Gacela::bootstrap(__DIR__);
+$setup = (new SetupGacela())
+    ->setConfig(function (ConfigBuilder $configBuilder): void {
+        $configBuilder->add('config/*.php', 'config/local.php');
+    });
+
+Gacela::bootstrap(__DIR__, $setup);
 
 $facade = new CommentFacade();
 $score = $facade->getSpamScore('Lorem ipsum!');
